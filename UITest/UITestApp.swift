3793caf565061e82c0f5781ffbf30c4a8684a688
gravilaxe <joshuawright3132@outlook.com>
2023-01-20 17:07:02 +0000

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Joshua Wright on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
